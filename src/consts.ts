09e1266d3f549260ba59ab56fb57dc5d94d54f83
mkhsu2002 <mkhsu2002@gmail.com>
2026-04-29 19:34:18 -0700

    feat: initialize brand styles and 8 core landing pages

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Canary Smile Guide';
+export const SITE_DESCRIPTION = 'Dental Care Knowledge for Families in Surrey & Greater Vancouver';
